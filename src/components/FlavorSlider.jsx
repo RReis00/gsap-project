fdd89bf4f204e821f5c3f4161757c0d0b504146e
Rodri <136265952+RReis00@users.noreply.github.com>
2025-08-27 10:27:43 +0100

    some development

--- src/components/FlavorSlider.jsx
+++ src/components/FlavorSlider.jsx
@@ -1,15 +1,48 @@
+import { useGSAP } from "@gsap/react";
 import { flavorlists } from "../constants";
+import gsap from "gsap";
 
 const FlavorSlider = () => {
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "2% top",
+        end: "+=500px",
+        scrub: true,
+      },
+    });
+  });
+
   return (
     <div className="slider-wrapper">
       <div className="flavors">
-        {flavorlists.map((flavor) => {
+        {flavorlists.map((flavor) => (
           <div
             key={flavor.name}
             className={`relative z-30 lg:w-[50vw] w-96 lg:h-[70vh] md:w-[90vw] md:h-[50vh] h-80 flex-none ${flavor.rotation}`}
-          ></div>;
-        })}
+          >
+            <img
+              src={`/images/${flavor.color}-bg.svg`}
+              alt=""
+              className="absolute bottom-0"
+            />
+
+            <img
+              src={`/images/${flavor.color}-drink.webp`}
+              alt=""
+              className="drinks"
+            />
+
+            <img
+              src={`/images/${flavor.color}-elements.webp`}
+              alt=""
+              className="elements"
+            />
+
+            <h1>{flavor.name}</h1>
+          </div>
+        ))}
       </div>
     </div>
   );
